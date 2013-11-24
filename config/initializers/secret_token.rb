7efcc8b8161087bed9204b3223e4ae86a5fcce9d
Sunil Rao <sunil2rao@gmail.com>
2013-11-24 16:52:31 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketRite::Application.config.secret_key_base = '52da1c01fc6f56494abc302dd47f7aa7d367dee9232343ff85ea214d686075fd5eb2e8829ffe863984b836a90ff37bd473801ef5613ca8e994492c3e0f78b5a0'
