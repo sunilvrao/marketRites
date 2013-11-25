b54cc9fc44c455eaa28578fbe620f811d09af58b
Sunil Rao <sunil2rao@gmail.com>
2013-11-25 10:44:37 -0600

    Dashboard changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+  def after_sign_in_path_for(resource)
+    dashboard_index_path(resource)
+  end
+
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,5 +5,9 @@ class RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
   end
+  
+  def after_sign_up_path_for(resource)
+    dashboard_index_path(resource)
+  end
 
 end
